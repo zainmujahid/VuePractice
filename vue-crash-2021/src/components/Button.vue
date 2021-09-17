16a290eb0bc7c7fba0e4e5891e466adc970bc5d3
zain <zainmujahid01@gmail.com>
2021-09-17 14:22:51 +0500

    Completed Button Componenet

--- vue-crash-2021/src/components/Button.vue
+++ vue-crash-2021/src/components/Button.vue
@@ -1,9 +1,15 @@
 <template>
-    <button></button>
+    <button :style="{background: color}" class="btn">{{ text }}</button>
 </template>
 
 <script>
-
+export default {
+    name: "Button",
+    props: {
+        text: String,
+        color: String,
+    }
+}
 </script>
 
 <style scoped>
